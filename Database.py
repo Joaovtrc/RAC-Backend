ef124c6a5b800d013e11fc48e5fa1c2c016e665f
João Vitor <jvromerocruz@gmail.com>
2018-07-10 22:49:22 -0300

    added response methods; implemented responses on some methods; added insertDelete Answer;

--- Database.py
+++ Database.py
@@ -9,8 +9,32 @@ def insertEdit(obj):
     session.commit()
     session.close()
 
+
+#Intent
 def getIntents():
     session = DBSession()
     intents = session.query(Intent).all()
     session.close()
     return intents
+
+def getSingleIntent(id):
+    session = DBSession()
+    intent = session.query(Intent).filter_by(id=id).first()
+    session.close()
+    return intent
+
+def deleteIntent(id):
+    session = DBSession()
+    intent = session.query(Intent).filter_by(id=id).first()
+    session.delete(intent)
+    session.commit()
+    session.close()
+
+#Answer
+
+def deleteAnswer(id):
+    session = DBSession()
+    response = session.query(Response).filter_by(id=id).first()
+    session.delete(response)
+    session.commit()
+    session.close()
--- question-controller.py
+++ question-controller.py
@@ -3,14 +3,16 @@ import json
 import jsonpickle
 
 #FLASK
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, Response
 from flask import jsonify
 from flask_cors import CORS, cross_origin
+from sqlalchemy import exc
+
 
 #DB
 from DBClasses import Intent, Pattern, Response, IntentSchema, ResponseSchema, PatternSchema
 from marshmallow import pprint
-from Database import insertEdit, getIntents
+from Database import insertEdit, getIntents, getSingleIntent, deleteIntent, deleteAnswer
 
 #ChatBot&Training
 from Training import train
@@ -20,9 +22,35 @@ app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
+#Responses
+def returnOk():
+    message = {
+            'status': 200,
+            'message': 'Success',
+    }
+    resp = jsonify(message)
+    resp.status_code = 200
+
+    return resp
+
+@app.errorhandler(400)
+def errorResp(error=None):
+    message = {
+            'status': 400,
+            'message': 'Error: ' + request.url,
+    }
+    resp = jsonify(message)
+    resp.status_code = 400
+    return resp
+
+
+#Routes
 @app.route("/api/ChatBot/Train", methods=["GET", "POST"])
+@cross_origin()
 def trainChatbot():
     train()
+
+    #IMPLEMENTAR RESPOSTA COM CONTENT TYPE CERTO!!!!!!!!!!!!!!
     return "Ok"
 
 #INTENT
@@ -38,16 +66,46 @@ def listIntents():
 
 
 @app.route("/api/ChatBot/Response",methods=["POST"])
+@cross_origin()
 def responseChatbot():
     question = request.get_json()
     print(question['question'])
     classi = chatBotClassify(question['question'])
     print(classi)  
-    return json.dumps(dict(response = chatBotResponse(question['question']), classify = classi))
+    return json.dumps(dict(response = chatBotResponse(question['question'])))
 
 
 
 @app.route("/api/insertIntent",methods=["POST"])
+@cross_origin()
+def insertIntentAnswersPatterns():
+    intents = request.get_json()
+    print(intents)
+    
+    intent = getSingleIntent(intents['id'])
+
+    for resJson in intents['responses']:
+        if(resJson['id'] == -1):
+            print(resJson)
+            res = Response()
+            res.response = resJson['response']
+            intent.responses.append(res)
+
+    for pattJson in intents['patterns']:
+        if(pattJson['id'] == -1):
+            patt = Pattern()
+            patt.pattern = pattJson['pattern']
+            intent.patterns.append(patt)
+
+
+    try:
+        insertEdit(intent)
+        return returnOk()
+
+    except exc.SQLAlchemyError:
+        return errorResp()
+    
+@app.route("/api/insertIntentJson",methods=["POST"])
 def insertIntent():
     intents = request.get_json()
     print(intents)
@@ -70,7 +128,17 @@ def insertIntent():
     
     return "Ok" 
 
+@app.route("/api/deleteAnswer/<int:id>",methods=["DELETE"])
+@cross_origin()
+def delAnswer(id):
+    try:
+        deleteAnswer(id)
+        return returnOk()
 
+    except exc.SQLAlchemyError:
+        return errorResp()
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
